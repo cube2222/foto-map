de989017604b81631251581be55991f19c966f80
Jakub Martin <jakub.wit.martin@gmail.com>
2020-11-14 16:23:59 +0100

    Moving forward.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, FormControl, InputGroup} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useRef, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [location, setLocation] = useState(null);
+    const addressInput = useRef(null);
+
+    var addressContent;
+    if (location != null) {
+        addressContent = <div>
+            Address:
+            <ul>
+                <li>Lat: {location[0]}</li>
+                <li>Lon: {location[1]}</li>
+            </ul>
+        </div>
+
+    } else {
+        addressContent = <div>
+            Please provide your address...
+        </div>
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/37/Solar_Panels.jpg" className="App-logo" alt="logo"/>
+                <div>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Twój Adres"
+                            aria-label="Twój Adres"
+                            aria-describedby="basic-addon2"
+                            id="addressInput"
+                            ref={addressInput}
+                        />
+                        <InputGroup.Append>
+                            <Button variant="outline-primary" onClick={
+                                (click) => {
+                                    if (addressInput.current.value.length > 0) {
+                                        fetch(`https://eu1.locationiq.com/v1/search.php?key=pk.7b5ef7afb5ebab795d1d41722b9c4f08&q=${encodeURIComponent(addressInput.current.value)}&format=json`)
+                                            .then(res => res.json())
+                                            .then(
+                                                (result) => {
+                                                    setLocation([result[0].lat, result[0].lon])
+                                                },
+                                                (error) => {
+                                                    console.log(error);
+                                                }
+                                            )
+                                    }
+                                }
+                            }>Oblicz</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </div>
+                {addressContent}
+            </header>
+        </div>
+    );
 }
 
 export default App;
